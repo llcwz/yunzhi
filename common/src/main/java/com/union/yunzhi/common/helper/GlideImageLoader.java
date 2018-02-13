efd51804ec1a4775e61fde5d4bcb59f84ccca662
JC9420 <35861736+JC9420@users.noreply.github.com>
2018-02-13 18:09:50 +0800

    修改GlideImageLoader并上传隐藏布局类HiddenAnimUtils
    用法见注释

--- common/src/main/java/com/union/yunzhi/common/helper/GlideImageLoader.java
+++ common/src/main/java/com/union/yunzhi/common/helper/GlideImageLoader.java
@@ -1,7 +1,6 @@
 package com.union.yunzhi.common.helper;
 
 import android.content.Context;
-import android.net.Uri;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -14,11 +13,11 @@ import com.youth.banner.loader.ImageLoader;
 public class GlideImageLoader extends ImageLoader {
     @Override
     public void displayImage(Context context, Object path, ImageView imageView) {
+
+        /**
+         * 根据提供的path将图片加载到ImageViews上
+         */
         Glide.with(context).load(path).into(imageView);
 
-
-        //用fresco加载图片简单用法，记得要写下面的createImageView方法
-        Uri uri = Uri.parse((String) path);
-        imageView.setImageURI(uri);
     }
 }
