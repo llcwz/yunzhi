3e7758c0a9bd56f64608c68954c1e1276de65e60
zhengmeng <782799981@qq.com>
2018-03-12 10:24:11 +0800

    优化了首启界面，增加了跳过选项
    优化了首启界面，增加了跳过选项

--- app/src/main/java/com/union/yunzhi/yunzhi/activities/LoadingActivity.java
+++ app/src/main/java/com/union/yunzhi/yunzhi/activities/LoadingActivity.java
@@ -3,17 +3,21 @@ package com.union.yunzhi.yunzhi.activities;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.union.yunzhi.common.app.PermissionsActivity;
 import com.union.yunzhi.yunzhi.MainActivity;
 import com.union.yunzhi.yunzhi.R;
 import com.union.yunzhi.yunzhi.manager.SPManager;
 
-public class LoadingActivity extends PermissionsActivity {
+public class LoadingActivity extends PermissionsActivity implements View.OnClickListener {
 
-    private RelativeLayout mAdLayout;
+    private FrameLayout mAdLayout;
     private RelativeLayout mCopyLayout;
+    private TextView skipButton;
 
     private Handler mHandler = new Handler() {
         @Override
@@ -41,8 +45,10 @@ public class LoadingActivity extends PermissionsActivity {
     protected void initWidget() {
 
 
-        mAdLayout = (RelativeLayout) findViewById(R.id.ad_content_view);
+        mAdLayout = (FrameLayout) findViewById(R.id.ad_content_view);
         mCopyLayout = (RelativeLayout) findViewById(R.id.content_layout);
+        skipButton = (TextView) findViewById(R.id.btn_skip);
+        skipButton.setOnClickListener(this);
 
         /**
          * 预留广告加载
@@ -60,5 +66,16 @@ public class LoadingActivity extends PermissionsActivity {
     protected void onDestroy() {
         super.onDestroy();
         mHandler.removeCallbacksAndMessages(null);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_skip:
+                startActivity(new Intent(LoadingActivity.this,
+                        MainActivity.class));
+                finish();
+                break;
+        }
     }
 }
