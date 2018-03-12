150ceef7e2474a951283be620df8902e1a59a7d7
zhengmeng <782799981@qq.com>
2018-03-12 21:40:23 +0800

    推送
    推送

---
+++ factories/src/main/java/com/union/yunzhi/factories/moudles/jpush/PushMessage.java
@@ -0,0 +1,16 @@
+package com.union.yunzhi.factories.moudles.jpush;
+
+import com.union.yunzhi.factories.moudles.BaseModel;
+
+/**
+ * Created by meng on 2018/3/12.
+ */
+
+public class PushMessage extends BaseModel {
+
+    // 消息类型
+    public String messageType = null;
+
+    // 详情内容
+    public String messageContent = null;
+}
