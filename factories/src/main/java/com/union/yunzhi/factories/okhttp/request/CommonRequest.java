66a5667355bd4077a1565fb33335056dead4d336
zhengmeng <782799981@qq.com>
2018-03-14 20:30:12 +0800

    提交修改
    提交修改

--- factories/src/main/java/com/union/yunzhi/factories/okhttp/request/CommonRequest.java
+++ factories/src/main/java/com/union/yunzhi/factories/okhttp/request/CommonRequest.java
@@ -55,11 +55,16 @@ public class CommonRequest {
         StringBuilder value = new StringBuilder();
 
         if(params != null){
+            value.append("{");
             for(Map.Entry<String,String> entry:params.urlParams.entrySet()){
                 //将请求参数逐一遍历添加到我们的请求构建中
                 //mFormBodyBuild.add(entry.getKey(),entry.getValue());
                 value.append(entry.getValue());
+                value.append(",");
             }
+           // urlBuilder.substring(0,urlBuilder.length()-1)
+            value.substring(0,value.length()-1);
+            value.append("}");
             mFormBodyBuild.add("data",value.toString());
 
         }
