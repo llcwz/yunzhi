4b0fca0a0e8c421b4b1b6e59e04832ed24cb88e2
李德贵 <1012610779@qq.com>
2018-03-17 18:40:19 +0800

    布局改变

--- app/src/main/java/com/union/yunzhi/yunzhi/activities/classfication/ClassCourseDetailsActivity.java
+++ app/src/main/java/com/union/yunzhi/yunzhi/activities/classfication/ClassCourseDetailsActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
@@ -43,12 +46,12 @@ import java.util.List;
 public class ClassCourseDetailsActivity extends ActivityM implements View.OnClickListener {
 
 
+
+    private Toolbar mToolbar;
+
     //课程简介视频
-    private MyScrollView mMyScrollView;
-    private FrameLayout hiddenView;
     private LinearLayout mLinearLayout;
-    private ConstraintLayout showView;
-    private RoundedImageView back,share,play,xback,xshare;
+    private RoundedImageView play,xback,xshare;
     private TextView mLikeCount,courseName,courseTeacher;
     private ImageButton mLike;
     private ImageView videoCover;
@@ -99,25 +102,22 @@ public class ClassCourseDetailsActivity extends ActivityM implements View.OnClic
         /**
          * 视频简介部分
          */
-        mMyScrollView= (MyScrollView) findViewById(R.id.scroll_details);
-        hiddenView= (FrameLayout) findViewById(R.id.lv_details_video);
-        showView= (ConstraintLayout) findViewById(R.id.lv_details_head);
+        mToolbar= (Toolbar) findViewById(R.id.lv_details_head);
         mLikeCount= (TextView) findViewById(R.id.tv_upCount);
         mLike= (ImageButton) findViewById(R.id.img_like);
-        showView.setVisibility(View.GONE);
         videoCover= (ImageView) findViewById(R.id.iv_course_details_video);
         courseName= (TextView) findViewById(R.id.tv_big_title);
         courseTeacher= (TextView) findViewById(R.id.tv_small_title);
         play= (RoundedImageView) findViewById(R.id.rImagV_play);
-        back= (RoundedImageView) findViewById(R.id.rImagV_back);
-        share= (RoundedImageView) findViewById(R.id.rImagV_share);
         xback= (RoundedImageView) findViewById(R.id.rImgV_back_hidden);
         xshare= (RoundedImageView) findViewById(R.id.rImgV_share_hidden);
-        back.setOnClickListener(this);xback.setOnClickListener(this);
-        share.setOnClickListener(this);xshare.setOnClickListener(this);
+
         play.setOnClickListener(this);
+        xback.setOnClickListener(this);
+        xshare.setOnClickListener(this);
 
-
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
 
         /**
          * 课程简介部分
@@ -168,31 +168,31 @@ public class ClassCourseDetailsActivity extends ActivityM implements View.OnClic
          * 其第二个参数“48”为View的高度，单位为dp
          * 但是不能通过View.getHeight()获得，view 对象动态渲染高度会有变化
          */
-        mMyScrollView.setOnScollChangedListener(new MyScrollView.OnScollChangedListener() {
-            @Override
-            public void onScrollViewChanged(MyScrollView scrollView, int x, int y, int oldx, int oldy) {
-
-                //LogUtils.d("KKK","滚动监听");
-                //手指上滑
-                if((y>=(hiddenView.getHeight()-200))&&y-oldy>8){
-                    if(hiddenView.getVisibility()==View.VISIBLE){
-                        LogUtils.d("KKK","执行View隐藏");
-                        //HiddenAnimUtils.newInstance(getBaseContext(),224).closeAnimate(hiddenView);
-                        hiddenView.setVisibility(View.GONE);
-                        HiddenAnimUtils.newInstance(getBaseContext(),48).openAnimate(showView);
-                    }
-                } else if(scrollView.getScrollY()==0){//下滑到顶
-                    if(hiddenView.getVisibility()==View.GONE){
-                        LogUtils.d("KKK","执行View显现");
-                        HiddenAnimUtils.newInstance(getBaseContext(),224).openAnimate(hiddenView);
-                    }
-                    //HiddenAnimUtils.newInstance(getBaseContext(),48).closeAnimate(showView);
-                    showView.setVisibility(View.GONE);
-                }else{
-
-                }
-            }
-        });
+//        mMyScrollView.setOnScollChangedListener(new MyScrollView.OnScollChangedListener() {
+//            @Override
+//            public void onScrollViewChanged(MyScrollView scrollView, int x, int y, int oldx, int oldy) {
+//
+//                //LogUtils.d("KKK","滚动监听");
+//                //手指上滑
+//                if((y>=(hiddenView.getHeight()-200))&&y-oldy>8){
+//                    if(hiddenView.getVisibility()==View.VISIBLE){
+//                        LogUtils.d("KKK","执行View隐藏");
+//                        //HiddenAnimUtils.newInstance(getBaseContext(),224).closeAnimate(hiddenView);
+//                        hiddenView.setVisibility(View.GONE);
+//                        HiddenAnimUtils.newInstance(getBaseContext(),48).openAnimate(showView);
+//                    }
+//                } else if(scrollView.getScrollY()==0){//下滑到顶
+//                    if(hiddenView.getVisibility()==View.GONE){
+//                        LogUtils.d("KKK","执行View显现");
+//                        HiddenAnimUtils.newInstance(getBaseContext(),224).openAnimate(hiddenView);
+//                    }
+//                    //HiddenAnimUtils.newInstance(getBaseContext(),48).closeAnimate(showView);
+//                    showView.setVisibility(View.GONE);
+//                }else{
+//
+//                }
+//            }
+//        });
 
         //请求并适配页面数据
         requestAllData(courseid,teacherid);
@@ -317,11 +317,9 @@ public class ClassCourseDetailsActivity extends ActivityM implements View.OnClic
                 Intent intent=new Intent(ClassCourseDetailsActivity.this,ClassCourseFileActivity.class);
                 startActivity(intent);
                 break;
-            case R.id.rImagV_back:
             case R.id.rImgV_back_hidden:
                 finish();
                 break;
-            case R.id.rImagV_share:
             case R.id.rImgV_share_hidden:
                 break;
             case R.id.rImagV_play:
